d7115f1671ef3178af9a2c5cee529ed84eca2e13
Aditya Pathak <adityapathak@Adityas-MacBook-Pro.local>
2022-12-22 19:22:40 +0530

    task 1

--- app/server.go
+++ app/server.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"log"
+	"net"
+)
+
+const (
+	HOST = "localhost"
+	PORT = "6379"
+	TYPE = "tcp"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	log.Println("Logs from your program will appear here!")
+	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Panic("Failed to bind to port 6379 ", err)
+	}
+	defer listen.Close()
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	_, err = listen.Accept()
+	if err != nil {
+		log.Panic("Error accepting connection: ", err)
+	}
 }
